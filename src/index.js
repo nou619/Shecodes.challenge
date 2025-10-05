bb4b42ce829d0f36604d321241f48ddbed56470d
nour ben hnia <nourbenhnia619@gmail.com>
2025-10-05 21:40:57 +0200

    fixed api

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ function displayTemperature(response) {
   temperatureElement.innerHTML = temperature;
 }
 
+
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "62b63b2t79f05a00f231dof1d4ddeeaa";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
